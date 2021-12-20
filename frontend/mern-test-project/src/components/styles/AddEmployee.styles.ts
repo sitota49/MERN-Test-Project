e4942466d595bf058238ad4ad7fd66d8991d7077
sitota49 <se.sitota.ezra@gmail.com>
2021-12-20 15:56:35 +0300

    final changes

---
+++ frontend/mern-test-project/src/components/styles/AddEmployee.styles.ts
@@ -0,0 +1,8 @@
+import { Accordion, Button } from "react-bootstrap";
+import styled from "styled-components";
+
+const StyledSubmitButton = styled(Button)`
+  float: right;
+`;
+
+export { StyledSubmitButton };
